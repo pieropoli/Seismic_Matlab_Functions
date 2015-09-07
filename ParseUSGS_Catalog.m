ef2b8bfa5544cbdd8c8f5a65d542a3c8cc2a388d
pieropoli <pieropoli@pieropolis-MacBook-Pro-2.local>
2015-09-07 12:45:25 -0400

    update

--- ParseUSGS_Catalog.m
+++ ParseUSGS_Catalog.m
@@ -1,5 +1,7 @@
 function [catalog] = ParseUSGS_Catalog(catalog)
 % need to be finished...
+% Function to parse the usgs catalog as downloaded ins CSV format
+
 id = fopen(char(catalog));
 [catalog] =textscan(id,'%s %f %f %f %f %s%*[^\n]','Delimiter',',','EmptyValue',0,'HeaderLines',1); 
 fclose(id);
